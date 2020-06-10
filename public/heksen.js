c114bb5555edbe3694bef0f1794cb860ce50256c
Lisan <lisannekraal@gmail.com>
2020-06-10 16:25:44 +0200

    also changed actually for last commit

--- public/heksen.js
+++ public/heksen.js
@@ -5,16 +5,20 @@ const accordion = document.getElementById("accordion");
 
 function initAccordion(accordionElem){
 
+    //for every element in this row, a default panel is created
+
     //when panel is clicked, handlePanelClick is called.          
     function handlePanelClick(event){
-        if (event.currentTarget.classList.contains("active")) {
-            hidePanel(event.currentTarget);
+        if (event.currentTarget.parentElement.classList.contains("active")) {
+            hidePanel(event.currentTarget.parentElement);
+            //HIER TOEVOEGEN AUDIOSOURCE WEER LEEG MAKEN?
         } else {
-            showPanel(event.currentTarget);
+            showPanel(event.currentTarget.parentElement);
+            //HIER TOEVOEGEN INLADEN AUDIO SOURCE?
         }
     }
   
-  //Hide currentPanel and show new panel.    
+  //show new panel & hide current one    
     function showPanel(panel){
       //Hide current one. First time it will be null. 
        var expandedPanel = accordionElem.querySelector(".active");
@@ -23,26 +27,25 @@ function initAccordion(accordionElem){
        } 
        //Show new one
        panel.classList.add("active");
+       //set source of audio player
+       var trackNumber = panel.querySelector(".acc-header").innerHTML;
+       panel.querySelector(".js-player").innerHTML = `<source src="/audio/${trackNumber}">`
+       console.log(panel.querySelector(".js-player").innerHTML);
     }
 
     function hidePanel(panel){
         panel.classList.remove("active");
+        panel.querySelector(".js-player").innerHTML = '<source src="defaultsource">';
     }
     
     //loop over the panels to create event listeners that fire the function
     var allPanelElems = accordionElem.querySelectorAll(".panel");
-    for (var i = 0, len = allPanelElems.length; i < len; i++){
-         allPanelElems[i].addEventListener("click", handlePanelClick);
+    var allHeaderElems = accordionElem.querySelectorAll(".acc-header");
+    for (var i = 0, len = allHeaderElems.length; i < len; i++){
+        allHeaderElems[i].addEventListener("click", handlePanelClick);
     }
-  
     //By Default Show first panel
-    showPanel(allPanelElems[0])
+    //showPanel(allPanelElems[0])
 }
 
-//call the function so it is in use
 initAccordion(accordion);
-
-
-
-//WISHLIST
-//try adding speed options (js-28)
